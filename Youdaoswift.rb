941b685aeb867cdfd6def058ece0a2466f8cd4b2
wangchi <wangchi@meican.com>
2018-05-24 15:22:45 +0800

    adapt to new api

--- Youdaoswift.rb
+++ Youdaoswift.rb
@@ -4,8 +4,8 @@ class Youdaoswift < Formula
 	url "https://youdaocli-swift-1256175899.cos.ap-beijing.myqcloud.com/YoudaoCLI-Swift.zip"
   sha256 "317ad4ee11a2b7195b10079e8e93ba1ef2b2753e18dc61d4b63c70500f3f004f"
 
-  depends_on "mpg123" => :run
-  depends_on "libxml2" => :run
+  depends_on "mpg123"
+  depends_on "libxml2"
 
   def install
     bin.install "YoudaoCLI-Swift"
